4fcf35bd1397d800a7306c2fd7f592a5a481f82d
Adv <tiffanydpro923@gmail.com>
2022-03-02 19:13:11 +0100

    maj project algo 01

---
+++ js/app.js
@@ -0,0 +1,14 @@
+import API from './scripts/Api.class'
+import Search from './scripts/Search.class'
+
+import urlData from '../data/receipts.json'
+
+async function app () {
+  const Api = new API(urlData)
+  const data = await Api.getAllReceipts()
+  const searchEngine = new Search(data)
+
+  searchEngine.init()
+}
+
+app()
